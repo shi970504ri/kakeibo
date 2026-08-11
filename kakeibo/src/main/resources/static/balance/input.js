2b1e78883308fa4d85b0ab5b4d6bbbb2cc7790c9
晴山 大樹 <ak.970504.ko@gmail.com>
2026-08-11 20:27:36 +0900

    202608112027_update_input.js内のコメント削除

--- kakeibo/src/main/resources/static/balance/input.js
+++ kakeibo/src/main/resources/static/balance/input.js
@@ -1,19 +1,14 @@
-//初期設定
 let nextNo;
 window.onload = () => {
 	nextNo = document.querySelectorAll(".detail_row").length + 1;
 };
-//画像アップロード
 async function uploadFile(input) {
-	//選択されたファイル取得
 	const file = input.files[0];
 	if (!file) {
 		return;
 	}
-	//ボタン表示変更
 	const button =  input.parentElement.querySelector(".file_btn");
 	button.innerHTML = `選択済み<br>再選択可能`;
-	//サーバーへ送信
 	const formData = new FormData();
 	formData.append("file", file);
 	try {
@@ -29,19 +24,13 @@ async function uploadFile(input) {
 		console.log("アップデート失敗");
 	}
 }
-//明細行追加
 function addRows(count) {
-	//明細一覧取得
 	const detailList = document.querySelector(".detail_list");
-	//現在の行数取得
 	let no = document.querySelectorAll(".detail_row").length;
 	for (let i = 0; i < count; i ++) {
-		//連番採番
 		no++;
-		//新規行生成
 		const row = document.createElement("div");
 		row.className = "detail_row";
-		//HTML生成
 		row.innerHTML = `
 			<p class="no">
 				No.${String(no).padStart(5, "0")}
@@ -78,25 +67,18 @@ function addRows(count) {
 				</button>
 			</div>
 		`;
-		//画面へ追加
 		detailList.appendChild(row);
 	}
 }
-//明細行削除
 function deleteRow(button) {
-	//現在の行数取得
 	const rows = document.querySelectorAll(".detail_row");
-	//最低1件は残す
 	if (rows.length <=1) {
 		alert("最低1件は残して下さい");
 		return;
 	}
-	//押下された行を削除
 	button.closest(".detail_row").remove();
-	//No.採番
 	renumberRows();
 }
-//No.振り直し
 function renumberRows() {
 	const rows = document.querySelectorAll(".detail_row");
 	rows.forEach((row, index) => {
